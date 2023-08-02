137604cd9dd14d2e38fcebb5f7c345e47bf31d18
ali afraz <a.afraz.1380@gmail.com>
2023-08-02 11:48:21 +0330

    add process schema. add dummy resolvers

---
+++ src/graphql/process.ts
@@ -0,0 +1,82 @@
+import {
+	extendType,
+	intArg,
+	list,
+	nonNull,
+	objectType,
+	stringArg,
+} from "nexus";
+import { NexusGenObjects } from "../../nexus-typegen";
+
+export const Process = objectType({
+	name: "Process",
+	definition(t) {
+		t.nonNull.string("id");
+		t.nonNull.list.nonNull.string("images");
+		t.string("output");
+		t.nonNull.string("state");
+		t.string("log");
+		t.nonNull.string("created_at");
+	},
+});
+
+const processes: NexusGenObjects["Process"][] = [
+	{
+		id: "1",
+		images: ["image1", "image2", "image3"],
+		output: "output",
+		state: "PENDING",
+		log: "log",
+		created_at: "created at",
+	},
+	{
+		id: "2",
+		images: ["image12", "image22", "imag2e3"],
+		output: "ouadfftput",
+		state: "DONE",
+		log: "losdfadfg",
+		created_at: "creatasdfdfed at",
+	},
+];
+
+export const ProcessQuery = extendType({
+	type: "Query",
+	definition(t) {
+		t.nonNull.list.nonNull.field("processes", {
+			type: "Process",
+			args: {
+				id: stringArg(),
+				state: stringArg(),
+			},
+			resolve(parent, args, context) {
+				const { id, state } = args;
+				return processes.filter((ps) => ps.state === state);
+			},
+		});
+	},
+});
+
+export const ProcessMutation = extendType({
+	type: "Mutation",
+	definition(t) {
+		t.nonNull.field("CreateProcess", {
+			type: "Process",
+			args: {
+				images: nonNull(list(nonNull(stringArg()))),
+				border: nonNull(intArg()),
+				bg_color: nonNull(stringArg()),
+			},
+			resolve(parent, args, context) {
+				// TODO: run real process and add it to datebase
+				processes.push({
+					id: (processes.length + 1).toString(),
+					created_at: new Date().toString(),
+					images: args.images,
+					state: "PENDING",
+				});
+
+				return processes[processes.length - 1];
+			},
+		});
+	},
+});
