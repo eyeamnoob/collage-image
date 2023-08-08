62e10fb0580d0052dbeb5f69e8b033f1c79bcb2a
ali afraz <a.afraz.1380@gmail.com>
2023-08-08 10:12:40 +0330

    a script for deleting old files added. but i don't know if i should run it as a cron job or something else.

---
+++ src/delete_files.js
@@ -0,0 +1,46 @@
+const AWS = require("aws-sdk");
+const moment = require("moment");
+require("dotenv").config();
+
+async function delete_files(bucket_name, days_threshold) {
+	const CONFIG = {
+		endpoint: process.env.ENDPOINT,
+		accessKeyId: process.env.ACCESS_KEY,
+		secretAccessKey: process.env.SECRET_KEY,
+		region: "default",
+	};
+
+	const s3 = new AWS.S3(CONFIG);
+
+	const threshold_date = moment().subtract(days_threshold, "days");
+	const list_params = {
+		Bucket: bucket_name,
+	};
+
+	const objects = await s3.listObjectsV2(list_params).promise();
+
+	for (const obj of objects.Contents || []) {
+		const last_mod = moment(obj.LastModified);
+
+		if (last_mod.isBefore(threshold_date)) {
+			const delete_params = {
+				Bucket: bucket_name,
+				Key: obj.Key,
+			};
+
+			await s3.deleteObject(delete_params).promise();
+			console.log(`Deleted file: ${obj.Key}`);
+		}
+	}
+}
+
+const bucket_name = process.env.BUCKET;
+const days_threshold = 3;
+
+delete_files(bucket_name, days_threshold)
+	.then(() => {
+		console.log("File deletion completed.");
+	})
+	.catch((error) => {
+		console.error("Error deleting files:", error);
+	});
